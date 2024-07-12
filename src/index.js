dc2b2636e25954ac097843ceb6429672aa5e905b
ricky871 <yinlu@live.com>
2024-07-12 09:13:33 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,14 +7,14 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
+  "docker.outskirts.top": dockerHub,
+  "quay.outskirts.top": "https://quay.io",
+  "gcr.outskirts.top": "https://gcr.io",
+  "k8s-gcr.outskirts.top": "https://k8s.gcr.io",
+  "k8s.outskirts.top": "https://registry.k8s.io",
+  "ghcr.outskirts.top": "https://ghcr.io",
+  "cloudsmith.outskirts.top": "https://docker.cloudsmith.io",
+  "ecr.outskirts.top": "https://public.ecr.aws",
 
   // staging
   "docker-staging.libcuda.so": dockerHub,
